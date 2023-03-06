7f47f70c787ce9e19ac386c460073bedd585c629
Pablo Orellana <pabloo@unitech.com.ar>
2023-03-05 21:27:28 -0300

    feature/listado-productos-vista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductosComponent } from './productos/productos.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
