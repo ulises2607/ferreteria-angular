138e1cb98f2f40a9a5ae0c786ddde406e371cb5f
Pablo Orellana <mellipablo.1992@gmail.com>
2023-04-29 17:05:57 -0300

    formulario crear Producto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProductosComponent } from './productos/productos.component';
 import {MatTableModule} from '@angular/material/table';
 import {MatCardModule} from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
+import {MatSelectModule} from '@angular/material/select';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatIconModule} from '@angular/material/icon';
 import {HttpClientModule} from '@angular/common/http';
+import { ProductosComponent } from './productos/productos.component';
+import { CrearProductoComponent } from './productos/crear-producto/crear-producto.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     ProductosComponent,
-    NavbarComponent
+    NavbarComponent,
+    CrearProductoComponent
   ],
   imports: [
     BrowserModule,
@@ -36,6 +40,8 @@ import {HttpClientModule} from '@angular/common/http';
     MatListModule,
     MatCheckboxModule,
     MatIconModule,
+    MatSelectModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/productos/crear-producto/crear-producto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-producto',
+  templateUrl: './crear-producto.component.html',
+  styleUrls: ['./crear-producto.component.css']
+})
+export class CrearProductoComponent {
+
+    public listCategoria = [];
+    public listSector = [];
+}
