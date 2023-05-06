ec54714908c0d9f41f1d304bc691a30359a6f57a
Pablo Orellana <mellipablo.1992@gmail.com>
2023-05-06 18:06:39 -0300

    Boton de subir Imagen

--- src/app/productos/crear-producto/crear-producto.component.ts
+++ src/app/productos/crear-producto/crear-producto.component.ts
@@ -7,6 +7,8 @@ import { Component,OnInit } from '@angular/core';
 })
 export class CrearProductoComponent implements OnInit{
 
+    public url_image:string = '';
+    public imagen!: File;
     public listCategoria = [];
     public listSector = [];
 
@@ -18,5 +20,17 @@ export class CrearProductoComponent implements OnInit{
       //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
       //Add 'implements OnInit' to the class.
 
+    }
+
+    selectFile(event:any){
+      if(event.target.files){
+        var reader =new FileReader();
+        const file = event.target.files[0];
+        this.imagen = file;
+        reader.readAsDataURL(event.target.files[0]);
+        reader.onload = (event:any) => {
+          this.url_image = event.target.result;
+        }
+      }
     }
 }
